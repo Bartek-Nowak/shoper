d08f6f652eb0ef0208d52fbecc0909f2db0837d6
Bartek <lenog5030@gmail.com>
2022-12-13 09:36:58 +0100

    Edit README

--- src/functions/checkDataEnd.js
+++ src/functions/checkDataEnd.js
@@ -2,6 +2,7 @@ import { LOAD_LIMIT } from "../const";
 
 export function checkDataEnd(prevData, nextData) {
   if (prevData === null) return false;
+  if(nextData.length === 0) return true;
   if (nextData.length % LOAD_LIMIT !== 0) return true;
   if (JSON.stringify(prevData) === JSON.stringify(nextData)) return true;
 }
